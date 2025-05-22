8b713262115e4033f2d7c171c6e388190cf8eebc
Dynamo <chickabamboombam@gmail.com>
2025-05-22 01:19:58 -0400

    Updated StarterGui/Menu/Anomalies/Chapters/ScrollingFrame/Zoo Crew/glitchEffect/ChangeEveryTime.lua

---
+++ StarterGui/Menu/Anomalies/Chapters/ScrollingFrame/Zoo Crew/glitchEffect/ChangeEveryTime.lua
@@ -0,0 +1,47 @@
+-- @ScriptType: LocalScript
+while true do wait(math.random(0.2,0.15)) --Random Wait
+	
+	local ScreenImage= script.Parent --Screen
+	
+	local RandomScreen= math.random(1,10) --Makes a random value
+	
+	if RandomScreen== 1 then --if the random value is 1
+		ScreenImage.Size= UDim2.new(1.1, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 2 then --if the random value is 2
+		ScreenImage.Size= UDim2.new(1.2, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 3 then --if the random value is 3
+		ScreenImage.Size= UDim2.new(1.3, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 4 then --if the random value is 4
+		ScreenImage.Size= UDim2.new(1.4, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 5 then --if the random value is 5
+		ScreenImage.Size= UDim2.new(1.5, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 6 then --if the random value is 6
+		ScreenImage.Size= UDim2.new(1.6, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 7 then --if the random value is 7
+		ScreenImage.Size= UDim2.new(1.7, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 8 then --if the random value is 8
+		ScreenImage.Size= UDim2.new(1.8, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 9 then --if the random value is 9
+		ScreenImage.Size= UDim2.new(1.9, 0,1.2, 0)
+	end
+	
+	if RandomScreen== 10 then --if the random value is 10
+		ScreenImage.Size= UDim2.new(2, 0,1.2, 0)
+	end
+end
