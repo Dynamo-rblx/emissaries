006c0a13eee9a2c6fa8ec6e19ae6b2261ac27a91
Dynamo <chickabamboombam@gmail.com>
2025-05-22 01:20:03 -0400

    Updated StarterGui/Menu/Shop/OnBoot.lua

---
+++ StarterGui/Menu/Shop/OnBoot.lua
@@ -0,0 +1,4 @@
+-- @ScriptType: LocalScript
+script.Parent:WaitForChild("Titles").Visible = false
+script.Parent:WaitForChild("Styles").Visible = false
+script.Parent:WaitForChild("Skills").Visible = true
